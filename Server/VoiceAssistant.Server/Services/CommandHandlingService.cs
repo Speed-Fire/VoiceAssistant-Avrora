b0a5e46a54ffe6b89fb8683e2db1af38b6541ed0
Vladislav Sidorovich <vlad44812@gmail.com>
2025-08-02 02:55:46 +0200

    Adding empty command handling service.

---
+++ Server/VoiceAssistant.Server/Services/CommandHandlingService.cs
@@ -0,0 +1,34 @@
+﻿using Google.Protobuf;
+using Grpc.Core;
+using VoiceAssistant.Server;
+
+namespace VoiceAssistant.Server.Services
+{
+	public class CommandHandlingService : CommandHandler.CommandHandlerBase
+	{
+		public override async Task Handle(
+			CommandRequest request,
+			IServerStreamWriter<CommandReply> responseStream,
+			ServerCallContext context)
+		{
+			string text = await ConvertToTextAsync(request.Audio);
+
+			List<string> commands = await RecognizeCommandsAsync(text);
+
+			foreach(var command in commands)
+			{
+				await responseStream.WriteAsync(new() { Command = command });
+			}
+		}
+
+		private async Task<List<string>> RecognizeCommandsAsync(string text)
+		{
+			throw new NotImplementedException();
+		}
+
+		private async Task<string> ConvertToTextAsync(ByteString audio)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
