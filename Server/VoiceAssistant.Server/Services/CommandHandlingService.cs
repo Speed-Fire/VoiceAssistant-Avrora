79c23adf03788221752c3cb53f1411efaee00596
Vladislav Sidorovich <vlad44812@gmail.com>
2025-08-03 19:20:21 +0200

    Adding basic logic to CommandHandlingService.

--- Server/VoiceAssistant.Server/Services/CommandHandlingService.cs
+++ Server/VoiceAssistant.Server/Services/CommandHandlingService.cs
@@ -1,34 +1,84 @@
 ﻿using Google.Protobuf;
 using Grpc.Core;
+using Renci.SshNet;
+using StackExchange.Redis;
+using System.Text.Json.Nodes;
 using VoiceAssistant.Server;
 
 namespace VoiceAssistant.Server.Services
 {
 	public class CommandHandlingService : CommandHandler.CommandHandlerBase
 	{
+		private readonly ConnectionMultiplexer _redis;
+		private readonly SftpClient _audioFTP;
+		private readonly string _recognitionQueueName;
+
+		public CommandHandlingService(
+			ConnectionMultiplexer redis,
+			[FromKeyedServices("AudioFTP")] SftpClient audioFTP,
+			IConfiguration config)
+		{
+			_redis = redis;
+			_audioFTP = audioFTP;
+			_recognitionQueueName = config["Redis:Queues:Recognition"]!;
+		}
+
 		public override async Task Handle(
 			CommandRequest request,
 			IServerStreamWriter<CommandReply> responseStream,
 			ServerCallContext context)
 		{
-			string text = await ConvertToTextAsync(request.Audio);
+			var taskId = Guid.NewGuid().ToString();
+			var audio = new MemoryStream();
+			request.Audio.WriteTo(audio);
 
-			List<string> commands = await RecognizeCommandsAsync(text);
+			var audio_url = await UploadAudioToFTP(taskId, audio, context.CancellationToken);
+			await EnqueueRecognitionTask(taskId, audio_url);
 
+			var commands = (await GetRecognitionResult(taskId)).Split('|');
+			
 			foreach(var command in commands)
 			{
 				await responseStream.WriteAsync(new() { Command = command });
 			}
 		}
 
-		private async Task<List<string>> RecognizeCommandsAsync(string text)
+		private async Task<string> UploadAudioToFTP(string filename, Stream audio, CancellationToken stoppingToken)
 		{
-			throw new NotImplementedException();
+			await _audioFTP.ConnectAsync(stoppingToken);
+
+			var path = filename;
+
+			_audioFTP.UploadFile(audio, path);
+
+			return path;
 		}
 
-		private async Task<string> ConvertToTextAsync(ByteString audio)
+		private async Task EnqueueRecognitionTask(string taskId, string audio_url)
 		{
-			throw new NotImplementedException();
+			var json = string.Format("{{\"Id\":\"{0}\":\"{1}\"}}", taskId, audio_url);
+
+			var db = _redis.GetDatabase();
+			await db.ListLeftPushAsync(_recognitionQueueName, json);
+		}
+
+		private async Task<string> GetRecognitionResult(string taskId)
+		{
+			var resultKey = $"{_recognitionQueueName}:{taskId}";
+			var db = _redis.GetDatabase();
+
+			for(int i = 0; i < 10; i++)
+			{
+				var result = await db.StringGetDeleteAsync(resultKey);
+				if (result != RedisValue.Null)
+				{
+					return result!;
+				}
+
+				await Task.Delay(1000);
+			}
+
+			return "Server timeout exceeded";
 		}
 	}
 }
