409e56a4e81203bfcfc6b61319296f71f9d66182
Vladislav Sidorovich <vlad44812@gmail.com>
2025-08-04 20:55:36 +0200

    Setting configuration source to environment variables.

--- Server/VoiceAssistant.Server.SpeechToText/Config.py
+++ Server/VoiceAssistant.Server.SpeechToText/Config.py
@@ -1,21 +1,22 @@
 
+import os
 
-redis_ip = "localhost"
-redis_port = 6379
-redis_password = ""
+redis_ip = os.getenv("REDIS_HOST")
+redis_port = int(os.getenv("REDIS_PORT"))
+redis_password = os.getenv("REDIS_PASSWORD")
 
-redis_recognition_queue_name = ""
-redis_temp_recognition_queue_name = ""
-redis_timestamps_recognition_queue_name = ""
+redis_recognition_queue_name = os.getenv("REDIS_QUEUES_RECOGNITION_PENDING")
+redis_temp_recognition_queue_name = os.getenv("REDIS_QUEUES_RECOGNITION_PROCESSING")
+redis_timestamps_recognition_queue_name = os.getenv("REDIS_QUEUES_RECOGNITION_TIMESTAMPS")
 
-redis_command_handling_queue_name = ""
-redis_temp_command_handling_queue_name = ""
-redis_timestamps_command_handling_queue_name = ""
+redis_command_handling_queue_name = os.getenv("REDIS_QUEUES_COMMAND_HANDLING_PENDING")
+redis_temp_command_handling_queue_name = os.getenv("REDIS_QUEUES_COMMAND_HANDLING_PROCESSING")
+redis_timestamps_command_handling_queue_name = os.getenv("REDIS_QUEUES_COMMAND_HANDLING_TIMESTAMPS")
 
-sftp_ip = "localhost"
-sftp_port = "2222"
-sftp_username = "AudioUser"
-sftp_password = "12345"
+sftp_ip = os.getenv("SFTP_HOST")
+sftp_port = int(os.getenv("SFTP_PORT"))
+sftp_username = os.getenv("SFTP_USERS_AUDIO_NAME")
+sftp_password = os.getenv("SFTP_USERS_AUDIO_PASSWORD")
 
 
 
