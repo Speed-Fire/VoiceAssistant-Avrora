f038e114b95ece325e08df8925c6e5db90cf9f35
Vladislav Sidorovich <vlad44812@gmail.com>
2025-08-15 02:11:12 +0200

    Adding user data to gRPC command, changing task json, adding task data options.

---
+++ Server/VoiceAssistant.Server/Options/TasksDataOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace VoiceAssistant.Server.Options
+{
+	public class TasksDataOptions
+	{
+		[ConfigurationKeyName("REDIS_HASHES_TASKS_DESCRIPTION")]
+		public string TaskDescriptionMap { get; set; } = string.Empty;
+
+		[ConfigurationKeyName("REDIS_HASHES_TASKS_STATUS")]
+		public string TaskStatusMap { get; set; } = string.Empty;
+	}
+}
--- Server/VoiceAssistant.Server/Services/CommandHandlingService.cs
+++ Server/VoiceAssistant.Server/Services/CommandHandlingService.cs
@@ -4,6 +4,7 @@ using Renci.SshNet;
 using StackExchange.Redis;
 using System.Text.Json.Nodes;
 using VoiceAssistant.Server;
+using VoiceAssistant.Server.Extensions;
 using VoiceAssistant.Server.Options;
 
 namespace VoiceAssistant.Server.Services
@@ -12,15 +13,31 @@ namespace VoiceAssistant.Server.Services
 	{
 		private readonly ConnectionMultiplexer _redis;
 		private readonly SftpClient _audioFTP;
+		private readonly LoadedLuaScript _pushRecognitionTaskScript;
+		private readonly TasksDataOptions _tasksData;
 		private readonly RecognitionQueueOptions _recognitionQueue;
+
+		private const string JSON_TASK_FORMAT =
+			"""
+			{{
+				"Id": {0},
+				"User": {1},
+				"Status": 0,
+				"Content": {2}
+			}}
+			""";
 
 		public CommandHandlingService(
 			ConnectionMultiplexer redis,
 			[FromKeyedServices("AudioFTP")] SftpClient audioFTP,
+			[FromKeyedServices(DIConsts.KEY_LUA_PUSH_RECOGNITION_TASK)] LoadedLuaScript pushRecognitionTaskScript,
+			TasksDataOptions tasksData,
 			RecognitionQueueOptions recognitionQueue)
 		{
 			_redis = redis;
 			_audioFTP = audioFTP;
+			_pushRecognitionTaskScript = pushRecognitionTaskScript;
+			_tasksData = tasksData;
 			_recognitionQueue = recognitionQueue;
 		}
 
@@ -30,11 +47,12 @@ namespace VoiceAssistant.Server.Services
 			ServerCallContext context)
 		{
 			var taskId = Guid.NewGuid().ToString();
+			var user = request.User;
 			var audio = new MemoryStream();
 			request.Audio.WriteTo(audio);
 
 			var audio_url = await UploadAudioToFTP(taskId, audio, context.CancellationToken);
-			await EnqueueRecognitionTask(taskId, audio_url);
+			await EnqueueRecognitionTask(taskId, user, audio_url);
 
 			var commands = (await GetRecognitionResult(taskId)).Split('|');
 			
@@ -44,23 +62,39 @@ namespace VoiceAssistant.Server.Services
 			}
 		}
 
-		private async Task<string> UploadAudioToFTP(string filename, Stream audio, CancellationToken stoppingToken)
+		private async Task<string> UploadAudioToFTP(string taskid, Stream audio, CancellationToken stoppingToken)
 		{
 			await _audioFTP.ConnectAsync(stoppingToken);
 
-			var path = filename;
+			var path = taskid + ".mp3";
 
 			_audioFTP.UploadFile(audio, path);
 
 			return path;
 		}
 
-		private async Task EnqueueRecognitionTask(string taskId, string audio_url)
+		private async Task EnqueueRecognitionTask(string taskId, string user, string audio_url)
 		{
-			var json = string.Format("{{\"Id\":\"{0}\":\"{1}\"}}", taskId, audio_url);
+			var json = string.Format(JSON_TASK_FORMAT,
+				taskId, user, audio_url);
 
 			var db = _redis.GetDatabase();
-			await db.ListLeftPushAsync(_recognitionQueue.PendingQueue, json);
+
+			var keys = new RedisKey[]
+			{
+				_tasksData.TaskDescriptionMap,
+				_tasksData.TaskStatusMap,
+				_recognitionQueue.PendingQueue
+			};
+
+			var values = new RedisValue[]
+			{
+				taskId,
+				json
+			};
+
+			var result = await db.ScriptEvaluateAsync(_pushRecognitionTaskScript,
+				keys, values);
 		}
 
 		private async Task<string> GetRecognitionResult(string taskId)
